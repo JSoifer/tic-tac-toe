88a11ab52241ed6f6de48db00a621013462e4685
Student <example@example.com>
2018-11-28 12:45:49 -0800

    set up component tree and add starter code for tic tac toe tutorial.

---
+++ src/Square.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+  function Square(props) {
+    return (
+      <button className="square"
+        onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+
+export default Square;
